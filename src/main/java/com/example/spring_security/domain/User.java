6f0f6a59b3b97132f01f5d4c14772d0393bff87c
Chiwang Lee <122594223+noxknow@users.noreply.github.com>
2023-06-03 18:27:15 +0900

    Update User.java

--- src/main/java/com/example/spring_security/domain/User.java
+++ src/main/java/com/example/spring_security/domain/User.java
@@ -19,6 +19,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String userName; user_name
+    private String userName; 
     private String passWord;
 }
